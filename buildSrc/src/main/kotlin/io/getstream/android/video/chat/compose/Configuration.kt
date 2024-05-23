1184c83e7c73dc5d52fecd2e8ebc69bcc2287dd7
skydoves <skydoves@naver.com>
2024-05-23 09:49:01 +0900

    Update release version

--- buildSrc/src/main/kotlin/io/getstream/android/video/chat/compose/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/android/video/chat/compose/Configuration.kt
@@ -4,9 +4,9 @@ object Configuration {
     const val compileSdk = 34
     const val targetSdk = 34
     const val minSdk = 26
-    const val majorVersion = 0
-    const val minorVersion = 1
-    const val patchVersion = 0
+    const val majorVersion = 1
+    const val minorVersion = 0
+    const val patchVersion = 1
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 1
+    const val versionCode = 2
 }
